7be483fce41e92a4b6b21893ea6bab074419005a
chensenwei <chensenwei@meituan.com>
2018-05-08 15:20:25 +0800

    导出对象热加载

--- example/demo1.js
+++ example/demo1.js
@@ -1,8 +1,11 @@
 
 var path = require('path')
 var hot = require('../lib/hot')
+var tool = require('../lib/tool')
 
 var obj = require('./js/test')
+
+console.log('val', require('./js/test').val)
 
 hot(path.join(__dirname, './js/test'))
 
@@ -12,4 +15,6 @@ console.log('obj', obj)
 setInterval(function() {
     console.log('obj', obj)
 }, 2000)
+
+// setInterval(tool.showMemory, 5000)
  
--- example/js/test.js
+++ example/js/test.js
@@ -1,6 +1,12 @@
 
 
 
-var obj = {v: 12};
+var p1 = require('./p1')
+
+function test () {
+    return '1'
+}
+
+var obj = {v: '1', p1: test()};
 
 module.exports = obj;
