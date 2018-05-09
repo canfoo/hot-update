26b13a277972e427eb8ddd800ec5dace4cd5c77b
chensenwei <chensenwei@meituan.com>
2018-05-09 17:12:08 +0800

    demo1

--- example/demo1.js
+++ example/demo1.js
@@ -14,12 +14,10 @@ setTimeout(function() {  // 模拟修改文件内容
     console.log('文件修改了')
     fs.writeFile('./js/p1.js', "var obj = {name: 'hot-p222'}; module.exports = obj;");
 }, 2000);
-
 setTimeout(function() {  // 模拟修改文件内容
     console.log('文件修改了')
     fs.writeFile('./js/p1.js', "var obj = {name: 'hot-p333'}; module.exports = obj;");
 }, 4000);
-
 setTimeout(function() {  // 模拟修改文件内容
     console.log('文件修改了')
     fs.writeFile('./js/p1.js', "var obj = {name: 'hot-p111'}; module.exports = obj;");
